25fdfb2872c0a0ea32f1d4b9a4f204e41bf0da91
Spencer <2523081+spencerneutron@users.noreply.github.com>
2025-06-29 23:36:02 -0500

    Allow injecting CloudKit container in debug

--- Choreganize/ChoreganizeApp.swift
+++ Choreganize/ChoreganizeApp.swift
@@ -3,7 +3,14 @@ import SwiftUI
 @main
 struct ChoreganizeApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject private var model = AppModel()
+    @StateObject private var model: AppModel
+
+    init() {
+#if DEBUG
+        SharedCloudKitController.configure(container: MockCloudContainer())
+#endif
+        _model = StateObject(wrappedValue: AppModel())
+    }
 
     var body: some Scene {
         WindowGroup {
