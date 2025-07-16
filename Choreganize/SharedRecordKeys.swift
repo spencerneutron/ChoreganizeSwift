17294c5976c1c447060b8c6cf76a5bc90cd9f9a5
Spencer <2523081+spencerneutron@users.noreply.github.com>
2025-07-16 15:08:30 -0500

    Fix CloudKit parent reference action

--- Choreganize/SharedRecordKeys.swift
+++ Choreganize/SharedRecordKeys.swift
@@ -6,6 +6,11 @@ struct SharedRecordKeys {
     static let jsonKey = "json"
     static let lastEditedKey = "lastEdited"
     static let historyZoneName = "HistoryZone"
+    // Record type names
+    static let rootRecordType = "AppState"
+    static let choreRecordType = "Chore"
+    static let areaRecordType = "Area"
+    static let completionRecordType = "Completion"
     // Keys for persisting share information in UserDefaults
     static let savedShareRecordKey = "ckShareRecordName"
     static let savedRootRecordKey = "ckRootRecordName"
