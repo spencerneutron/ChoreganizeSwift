96d6a0979533fea1163e33789f8db5959de7582b
Spencer <2523081+spencerneutron@users.noreply.github.com>
2025-07-26 11:38:08 -0500

    Scope CloudKit fetches by parent

--- Choreganize/SharedCloudKitController.swift
+++ Choreganize/SharedCloudKitController.swift
@@ -13,7 +13,7 @@ protocol CloudDatabase {
     func llmDeleteRecord(withID id: CKRecord.ID) async throws
     func llmDeleteSubscription(withID id: String) async throws
     func save(_ subscription: CKSubscription) async throws -> CKSubscription
-    func llmAllRecords(ofType type: String, in zoneID: CKRecordZone.ID) async throws -> [CKRecord]
+    func llmAllRecords(ofType type: String, parentID: CKRecord.ID, in zoneID: CKRecordZone.ID) async throws -> [CKRecord]
 }
 
 protocol CloudContainer {
@@ -83,11 +83,21 @@ extension CKDatabase: CloudDatabase {
         }
     }
 
-    func llmAllRecords(ofType type: String, in zoneID: CKRecordZone.ID) async throws -> [CKRecord] {
-        let query = CKQuery(recordType: type, predicate: NSPredicate(value: true))
+    func llmAllRecords(ofType type: String, parentID: CKRecord.ID, in zoneID: CKRecordZone.ID) async throws -> [CKRecord] {
+        let parentRef = CKRecord.Reference(recordID: parentID, action: .none)
+        let predicate = NSPredicate(format: "parent == %@", parentRef)
+        let query = CKQuery(recordType: type, predicate: predicate)
 
-        let records = try await records(matching: query, inZoneWith: zoneID)
-        return records
+        var results: [CKRecord] = []
+        for try await result in records(matching: query, inZoneWith: zoneID) {
+            switch result {
+            case .success(let record):
+                results.append(record)
+            case .failure(let error):
+                throw error
+            }
+        }
+        return results
     }
 }
 
@@ -132,8 +142,12 @@ final class MockCloudDatabase: CloudDatabase {
         return subscription
     }
 
-    func llmAllRecords(ofType type: String, in zoneID: CKRecordZone.ID) async throws -> [CKRecord] {
-        return records.values.filter { $0.recordType == type && $0.recordID.zoneID == zoneID }
+    func llmAllRecords(ofType type: String, parentID: CKRecord.ID, in zoneID: CKRecordZone.ID) async throws -> [CKRecord] {
+        return records.values.filter {
+            $0.recordType == type &&
+            $0.recordID.zoneID == zoneID &&
+            $0.parent?.recordID == parentID
+        }
     }
 }
 
@@ -431,11 +445,12 @@ final class SharedCloudKitController: NSObject {
 
     /// Downloads all shared records and assembles an app state.
     func fetchSharedState() async throws -> AppModel.SavedState {
+        let rootID = try await ensureRootRecordID()
         _ = try await fetchOrCreateRootRecord()
         let zone = try await ensureZoneID()
-        let choreRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.choreRecordType, in: zone)
-        let areaRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.areaRecordType, in: zone)
-        let completionRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.completionRecordType, in: zone)
+        let choreRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.choreRecordType, parentID: rootID, in: zone)
+        let areaRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.areaRecordType, parentID: rootID, in: zone)
+        let completionRecords = try await sharedDB.llmAllRecords(ofType: SharedRecordKeys.completionRecordType, parentID: rootID, in: zone)
 
         let chores = choreRecords.compactMap(recordToChore)
         let areas = areaRecords.compactMap(recordToArea)
